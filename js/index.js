39b741ef2421067b13eb17ba5c98fc61ddd8921c
omarfarouq ahmed <omarfarouq.ahmed@student.moringaschool.com>
2023-08-17 17:33:05 +0300

    Done

--- js/index.js
+++ js/index.js
@@ -20,15 +20,15 @@ function adminLogin(username, password) {
 function howsTheWeather(temperature) {
   let response;
   if (temperature < 40) {
-    response = "brisk";
+    response = "brisk"
   } else if (temperature >= 40 && temperature <= 65) {
-    response = "a little chilly";
-  } else if (temperature > 85) {
-    response = "too dang hot";
+    response = "a little chilly"
+  } else if (temperature > 85) { 
+    response = "too dang hot"
   } else {
-    response = "perfect";
+    response = "perfect"
   }
-  return `It's ${response} out there!`;
+  return `It's ${response} out there!`
 }
 
 /* 
--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,73 @@
 #!/usr/bin/env python3
 
+
 def admin_login(username, password):
     # your code here
-    pass
+    if (
+        username.lower() == "admin"
+        and username.upper() == "ADMIN"
+        and password == "12345"
+    ):
+        return "Access granted"
+    else:
+        return "Access denied"
 
+
+# def hows_the_weather(temperature):
+#     # your code here
+#     if temperature < 40:
+#         response = "brisk"
+#     elif 40 <= temperature <= 65:
+#         response = "It's perfect out there!"
+#     elif temperature > 85:
+#         response = "too dang hot"
+#     else:
+#         response = "perfect"
+
+
+#     return f"It's {response} out there!"
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40:
+        response = "brisk"
+    elif 40 <= temperature <= 65:
+        response = "a little chilly"
+    elif temperature > 85:
+        response = "too dang hot"
+    else:
+        response = "perfect"
+
+    return f"It's {response} out there!"
+
 
 def fizzbuzz(num):
     # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
 
+
+# Write a method `calculator` that takes three arguments: an operation and two
+#   numbers. If the operation is one of the following: `+`, `-`, `*`, or `\`,
+#   return the value of calling the operation on the two numbers. Otherwise,
+#   output a message saying "Invalid operation!" and return `nil`.
 def calculator(operation, num1, num2):
     # your code here
-    pass
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            print("Division by zero is not allowed.")
+            return None
+    else:
+        print("Invalid operation!")
